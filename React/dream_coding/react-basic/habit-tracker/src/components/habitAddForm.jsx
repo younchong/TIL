cd711e8f01534240652daaded4957373f511fbda
younchong <ungun96@naver.com>
2021-12-14 01:07:36 +0900

    21.12.13 TIL React dream-coding

--- React/dream_coding/react-basic/habit-tracker/src/components/habitAddForm.jsx
+++ React/dream_coding/react-basic/habit-tracker/src/components/habitAddForm.jsx
@@ -1,29 +1,27 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class HabitAddForm extends PureComponent {
-  formRef = React.createRef();
-  inputRef = React.createRef();
-  onSubmit = (event) => {
+const HabitAddForm = memo(props => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
+  const onSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name)
+    const name = inputRef.current.value;
+    name && props.onAdd(name)
     //this.inputRef.current.value = "";
-    this.formRef.current.reset();
+    formRef.current.reset();
   }
 
-  render() {
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-          <input
-            ref={this.inputRef} 
-            type="text"
-            className="add-input" 
-            placeholder="Write Habit" 
-          />
-          <button className="add-button">Add</button>
-      </form>
-    );    
-  }
-}
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+        <input
+          ref={inputRef} 
+          type="text"
+          className="add-input" 
+          placeholder="Write Habit" 
+        />
+        <button className="add-button">Add</button>
+    </form>
+  );
+});
 
 export default HabitAddForm;
