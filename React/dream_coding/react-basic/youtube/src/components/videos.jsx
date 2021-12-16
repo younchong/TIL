9f0ac4cb1675831c8bbb628d1389d13a651376e7
younchong <ungun96@naver.com>
2021-12-16 17:07:02 +0900

    21.12.16 TIL React

--- React/dream_coding/react-basic/youtube/src/components/videos.jsx
+++ React/dream_coding/react-basic/youtube/src/components/videos.jsx
@@ -3,34 +3,38 @@ import {api_key, info} from './info';
 import Video from './video';
 
 const Videos = (props) => {
-const [urlList, setUrl] = useState([]);
-
-const optionParams={
-	q:"bts", //여기에 검색결과 
-	part:"snippet",
-	key:api_key,
-	maxResults:2
- };
-
-let url="https://www.googleapis.com/youtube/v3/search?";
-for(let option in optionParams){
-	url+=option+"="+optionParams[option]+"&";
-}
-useEffect(() => {
-  document.title={urlList}
-})
-/*
-fetch(url)
-.then(response => response.json())
-.then(json => json.items)
-.then(items => items.map(item => item.snippet.thumbnails.default.url))
-.then(urls => setUrl(urls))
-.catch(err => console.log(err));
-*/
-  return (
-    <>
-    </>
-  )
+  const [titles, setTitles] = useState([]);
+  const [urlList, setUrl] = useState([]);
+  let response = new Promise(resolve => resolve(info.items));
+  response
+  .then(items => items.map(item => item.snippet.thumbnails.default.url))
+  .then(urls => setUrl(urls));
+  /*
+  const optionParams={
+    q:"bts", //여기에 검색결과 
+    part:"snippet",
+    key:api_key,
+    maxResults:2
+   };
+  
+  let url="https://www.googleapis.com/youtube/v3/search?";
+  for(let option in optionParams){
+    url+=option+"="+optionParams[option]+"&";
+  }
+  
+  fetch(url)
+  .then(response => response.json())
+  .then(json => json.items)
+  .then(items => items.map(item => item.snippet.thumbnails.default.url))
+  .then(urls => setUrl(urls))
+  .catch(err => console.log(err));
+  */
+  
+    return (
+      <>
+        
+      </>
+    )
 };
 
 export default Videos;
