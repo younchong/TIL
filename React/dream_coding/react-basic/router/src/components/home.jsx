660781c039e75d7d00835ca7388fd66aa4df0d67
younchong <ungun96@naver.com>
2021-12-24 17:09:39 +0900

    21.12.24 TIL react router

---
+++ React/dream_coding/react-basic/router/src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = props => (
+  <>
+    <h1>Home</h1>
+    <button>go to Profile</button>
+  </>
+);
+  
+
+export default Home;
---
+++ React/dream_coding/react-basic/router/src/components/profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = props => (
+  <>
+    <h1>Profile</h1>
+    <button>go to Home</button>
+  </>
+);
+
+
+export default Profile;
