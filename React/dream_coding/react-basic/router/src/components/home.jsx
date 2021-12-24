c62237b9f8fcf7f126c806b6aa62389bdc1b0f08
younchong <ungun96@naver.com>
2021-12-25 00:01:43 +0900

    21.12.24 TIL react router

--- React/dream_coding/react-basic/router/src/components/home.jsx
+++ React/dream_coding/react-basic/router/src/components/home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Home = props => (
-  <>
-    <h1>Home</h1>
-    <button>go to Profile</button>
-  </>
-);
+const Home = props => {
+  const navigate = useNavigate();
+
+  return <>
+  <h1>Home</h1>
+  <button
+  onClick={() => {
+    navigate("/profile");
+  }}
+  >go to Profile</button>
+</>
+}
   
 
 export default Home;
--- React/dream_coding/react-basic/router/src/components/profile.jsx
+++ React/dream_coding/react-basic/router/src/components/profile.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Profile = props => (
-  <>
-    <h1>Profile</h1>
-    <button>go to Home</button>
+const Profile = props => {
+  const navigate = useNavigate();
+
+  return <>
+  <h1>Profile</h1>
+  <button
+  onClick={() => {
+    navigate("/");
+  }}
+  >go to Home</button>
   </>
-);
-
+}
 
 export default Profile;
