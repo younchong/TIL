e3f8eb83551069627d351eedb969764b93985a37
younchong <ungun96@naver.com>
2022-01-05 20:58:14 +0900

    22.01.05 TIL React with class set up

---
+++ React/dream_coding/react-basic/card-with-Ellie/src/components/header/header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = (props) => <header>header</header>
+
+export default Header;
---
+++ React/dream_coding/react-basic/card-with-Ellie/src/components/login/login.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Login = ({name}) => <button>{name}</button>
+
+export default Login;
