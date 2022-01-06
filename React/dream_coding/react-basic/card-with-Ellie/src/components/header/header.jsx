66d62470d5d8b2954e23d45cfec48c3736e1885c
younchong <ungun96@naver.com>
2022-01-06 20:00:09 +0900

    22.01.06 React project review

--- React/dream_coding/react-basic/card-with-Ellie/src/components/header/header.jsx
+++ React/dream_coding/react-basic/card-with-Ellie/src/components/header/header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import styles from "./header.module.css"
 
-const Header = (props) => <header>header</header>
+const Header = (props) => {
+  return (
+    <header className={styles.main}>
+      <img src="../images/logo.png" alt="logo" />
+      <p>Card Maker</p>
+      </header>
+  )
+}
 
 export default Header;
