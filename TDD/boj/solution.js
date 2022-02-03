23592714e156fa228f922f5b157fd404a5ae317a
younchong <ungun96@naver.com>
2022-02-03 12:20:12 +0900

    22.02.03 TIL Algorithm BOJ solution Greedy

--- TDD/boj/solution.js
+++ TDD/boj/solution.js
@@ -72,7 +72,7 @@ function solution() {
   
   return answer;
 }
-*/
+
 
 // 3273 두 수의 합, two - pointer
 function solution() {
@@ -100,8 +100,78 @@ function solution() {
       start++;
     }
   }
-  
   return count;
 }
+
+// 1904 BOJ DP
+function solution() {
+  const N = 4;
+  const dp = new Array(100001).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= N; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  console.log(dp[N]);
+}
+solution();
+// BOJ 1010 DP
+function solution() {
+  //const input = require("fs").readFileSync("./input.txt").toString().split("\n");
+  const input = [
+    3,
+  "2 2",
+  "1 5",
+  "13 29"
+  ];
+  const N = input[0] * 1;
+  const dp = Array.from(new Array(31), () => new Array().fill(0));
+  for (let y = 0; y <= 30; y++) {
+    for (let x = 0; x <= y; x++) {
+        if (x === 0 || x === y) {
+          dp[y][x] = 1;
+        } else {
+          dp[y][x] = dp[y - 1][x - 1] + dp[y -1][x];
+        }
+    }
+  }
+  let answer = [];
+  for (let i = 1; i <= N; i++) {
+    const [N, M] = input[i].split(" ").map(value => parseInt(value));
+    
+    answer.push(dp[M][N]);
+  }
+
+  return answer.join("\n")
+}
+*/
+// BOJ 1049
+function solution() {
+  const input = require("fs").readFileSync("./input.txt").toString().split("\n");
+  const [N, M] = input[0].split(" ").map(value => parseInt(value));
+  let cost = Infinity;
+  let minPack = Infinity;
+  let minSolo = Infinity;
+
+  for (let i = 1; i <= M; i++) {
+    const [pack, solo] = input[i].split(" ").map(value => parseInt(value));
+    minPack = Math.min(minPack, pack);
+    minSolo = Math.min(minSolo, solo);
+    if (N <= 6) {
+      cost = Math.min(cost, minPack, minSolo * N);
+    } else {
+      let ceil = Math.ceil(N / 6);
+      let floor = Math.floor(N / 6);
+      cost = Math.min(cost, minPack * floor + minSolo * (N - floor * 6), minPack * ceil, minSolo * N);
+    }
+
+  }
+  console.log(cost)
+  return cost;
+}
+
+solution();
 
 module.exports = solution;
--- TDD/boj/solution.test.js
+++ TDD/boj/solution.test.js
@@ -15,12 +15,27 @@ describe("Boj 2470", () => {
   })
 })
 
+describe("Boj 3273", () => {
+  it("solution", () => {
+    expect(solution()).toEqual(3);
+  })
+})
+
+
+
+const solution = require("./solution.js");
+
+describe("Boj 1010", () => {
+  it("solution", () => {
+    expect(solution()).toBe("1\n5\n67863915");
+  })
+})
 */
 
 const solution = require("./solution.js");
 
-describe("Boj 3273", () => {
+describe("Boj 1049", () => {
   it("solution", () => {
-    expect(solution()).toEqual(3);
+    expect(solution()).toBe(6);
   })
 })
