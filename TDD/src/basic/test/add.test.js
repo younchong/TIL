0ff4a334b31c070054f2bfceea231b56934d4030
younchong <ungun96@naver.com>
2022-03-27 18:12:39 +0900

    TIL정리

--- TDD/src/basic/test/add.test.js
+++
@@ -1,6 +0,0 @@
-const add = require("../add.js");
-
-test("adds 1 + 2 to equal 3", () => {
-  // 테스트 코드 작성
-  expect(add(1, 2)).toBe(3);
-});
--- TDD/src/mock/product/product_client.js
+++
@@ -1,9 +0,0 @@
-class ProductClient {
-  fetchItems() {
-    return fetch("http://example.com").then((response) => 
-    response.json()
-    );
-  }
-}
-
-module.exports = ProductClient;
